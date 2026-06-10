1a39ebd6a151463b62105bb7e6db7f87ae24dda2
josue <josuefig19@gmail.com>
2026-06-10 10:11:15 -0600

    Mostrar lista de gastos

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
   import Presupuesto from './components/Presupuesto.vue';
   import ControlPresupuesto from './components/ControlPresupuesto.vue';
   import Modal from './components/Modal.vue';
+  import Gasto from './components/Gasto.vue';
+
   import { generarId } from './helpers';
 
   import iconoNuevoGasto from './assets/img/nuevo-gasto.svg'
@@ -94,6 +96,16 @@
 
     <main v-if="presupuesto > 0">
 
+      <div class="listado-gastos contenedor">
+        <h2>{{ gastos.length > 0 ? 'Gastos' : 'No hay gastos' }}</h2>
+
+        <Gasto
+          v-for="gasto in gastos"
+          :key="gasto.id"
+          :gasto="gasto"
+        />
+      </div>
+
       <div class="crear-gasto">
         <img 
           :src="iconoNuevoGasto"
@@ -181,7 +193,7 @@
     box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1);
     background-color: var(--blanco);
     border-radius: 1.2rem;
-    
+    padding: 5rem;
   }
 
 
@@ -196,4 +208,12 @@
     cursor: pointer;
   }
 
+  .listado-gastos{
+    margin-top: 10rem;
+  }
+
+  .listado-gastos h2{
+    font-weight: 900;
+    color: var(--gris-oscuro);
+  }
 </style>
