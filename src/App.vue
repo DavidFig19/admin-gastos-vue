d9d5af9c1d5b46888c1701e012d8189eba929987
josue <josuefig19@gmail.com>
2026-07-08 09:27:18 -0600

    filtra datos por categoria

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-  import { ref, reactive, watch } from 'vue';
+  import { ref, reactive, watch, computed } from 'vue';
   import Presupuesto from './components/Presupuesto.vue';
   import ControlPresupuesto from './components/ControlPresupuesto.vue';
   import Modal from './components/Modal.vue';
+  import Filtros from './components/Filtros.vue';
   import Gasto from './components/Gasto.vue';
 
   import { generarId } from './helpers';
@@ -15,7 +16,8 @@
   })
   const presupuesto = ref(0);
   const disponible = ref(0);
-  const gastado = ref(0)
+  const gastado = ref(0);
+  const filtro = ref('');
 
   const gasto = reactive({
     nombre:'',
@@ -117,7 +119,14 @@
       ocultarModal()
     }
   }
-  
+
+  const gastosFiltrados = computed(() => {
+    if(filtro.value){
+      return gastos.value.filter(gasto => gasto.categoria === filtro.value);
+    }
+
+    return gastos.value;
+  });
 
 </script>
 
@@ -146,12 +155,15 @@
 
 
     <main v-if="presupuesto > 0">
+      <Filtros
+        v-model:filtro="filtro"
+      />
 
       <div class="listado-gastos contenedor">
-        <h2>{{ gastos.length > 0 ? 'Gastos' : 'No hay gastos' }}</h2>
+        <h2>{{ gastosFiltrados.length > 0 ? 'Gastos' : 'No hay gastos' }}</h2>
 
         <Gasto
-          v-for="gasto in gastos"
+          v-for="gasto in gastosFiltrados"
           :key="gasto.id"
           :gasto="gasto"
           @seleccionar-gasto="seleccionarGasto"
